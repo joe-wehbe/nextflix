e6b0e97245764c93a7a87a41dd74c03a50d0de63
Joe-Wehbe <joewehbe@yahoo.com>
2024-05-28 17:49:17 +0300

    Finished designing the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  activeTab: string = 'movies';
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    this.updateIndicator();
   }
 
+  switchTab(tab: string): void {
+    this.activeTab = tab;
+    this.updateIndicator();
+  }
+
+  updateIndicator(): void {
+    const indicator = this.elementRef.nativeElement.querySelector('.tab-indicator');
+    const activeTabElement = this.elementRef.nativeElement.querySelector(`.${this.activeTab}-tab`);
+
+    if (activeTabElement) {
+      const tabRect = activeTabElement.getBoundingClientRect();
+      const containerRect = this.elementRef.nativeElement.querySelector('.tabs-container').getBoundingClientRect();
+
+      const left = tabRect.left - containerRect.left;
+      const width = tabRect.width;
+
+      this.renderer.setStyle(indicator, 'left', `${left}px`);
+      this.renderer.setStyle(indicator, 'width', `${width}px`);
+    }
+  }
 }
