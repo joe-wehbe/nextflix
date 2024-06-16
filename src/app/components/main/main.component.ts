afe576b32cca18d85bd05269bddad69dc8836df9
Joe-Wehbe <joewehbe@yahoo.com>
2024-06-17 01:46:14 +0300

    Implemented API for tvshow details

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -11,21 +11,36 @@ interface Movie{
   cast: string[];
 }
 
+interface TvShow{
+  id: number;
+  name: string;
+  firstAirDate: string;
+  posterPath: string;
+  genres: string[];
+  overview: string;
+  cast: string[];
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  /***** MOVIEs VARIABLES *****/
   upcomingMovies: Movie[] = [];
-  upcomingTvShows: Movie[] = [];
+  selectedMovieId: number = -1;
+  selectedMovie!: Movie;
 
+  /***** TV SHOWS VARIABLES *****/
+  upcomingTvShows: TvShow[] = [];
+  selectedTvShowId: number = -1;
+  selectedTvShow!: TvShow;
+
+  /***** OTHER VARIABLES *****/
   activeTab: String = 'movies';
   genreId = 28;
-
   searchQuery: string = '';
-
-  selectedMovieId: number = -1;
 
   constructor(private tmdbService: ThemoviedbService) { }
 
@@ -33,6 +48,7 @@ export class MainComponent implements OnInit {
     this.getUpcomingMovies(this.genreId);
   }
 
+  /******************** MOVIES FUNCTIONS ********************/
   getUpcomingMovies(genreId: number): void {
   this.tmdbService.getUpcomingMovies(genreId)
     .subscribe({
@@ -42,8 +58,8 @@ export class MainComponent implements OnInit {
           .map((movie: { id: number; title: string; poster_path: string; release_date: string }) => ({
             id: movie.id,
             title: movie.title,
-            posterPath: movie.poster_path,
-            releaseDate: movie.release_date
+            releaseDate: movie.release_date,
+            posterPath: movie.poster_path
           }));
         if (this.searchQuery) {
           movies = movies.filter((movie: { title: string; }) => movie.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
@@ -56,7 +72,34 @@ export class MainComponent implements OnInit {
     });
   }
 
-  getUpcomingTvShows(genreId: number): void {
+  getMovieDetails(movieId: number) {
+    this.tmdbService.getMovieDetails(movieId)
+    .subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.selectedMovie = {
+          id: response.id,
+          title: response.title,
+          releaseDate: response.release_date,
+          posterPath: response.poster_path,
+          genres: response.genres,
+          overview: response.overview,
+          cast: response.credits.cast
+        };
+      },
+      error: (error) => {
+        console.error("Error getting movie details:", error);
+      }
+    });
+  }
+
+  selectMovie(id: number){
+    this.selectedMovieId = id;
+    this.getMovieDetails(this.selectedMovieId);
+  }
+
+  /******************** TV SHOWS FUNCTIONS ********************/
+  getUpcomingTVShows(genreId: number): void {
   this.tmdbService.getUpcomingTVShows(genreId)
     .subscribe({
       next: (response) => {
@@ -64,9 +107,9 @@ export class MainComponent implements OnInit {
         let tvShows = response.results.filter((tvShow: { poster_path: null; }) => tvShow.poster_path !== null)
           .map((tvShow: { id: number; name: string; poster_path: string; first_air_date: string }) => ({
             id: tvShow.id,
-            title: tvShow.name,
-            posterPath: tvShow.poster_path,
-            releaseDate: tvShow.first_air_date
+            name: tvShow.name,
+            firstAirDate: tvShow.first_air_date,
+            posterPath: tvShow.poster_path
           }));
         if (this.searchQuery) {
           tvShows = tvShows.filter((tvShow: { title: string; }) => tvShow.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
@@ -79,42 +122,53 @@ export class MainComponent implements OnInit {
     });
   }
 
-  getMovieDetails(movieId: number){
-    this.tmdbService.getMovieDetails(movieId)
+  getTvShowDetails(tvShowId: number){
+    this.tmdbService.getTVShowDetails(tvShowId)
     .subscribe({
-      next: (response) => {
+      next: (response: any) => {
         console.log(response);
+        this.selectedTvShow = {
+          id: response.id,
+          name: response.name,
+          firstAirDate: response.first_air_date,
+          posterPath: response.poster_path,
+          genres: response.genres,
+          overview: response.overview,
+          cast: response.credits.cast
+        };
       },
       error: (error) => {
-        console.log("Error getting movie details");
+        console.error("Error getting TV show details:", error);
       }
     });
   }
 
+  selectTVShow(id: number){
+    this.selectedTvShowId = id;
+    this.getTvShowDetails(this.selectedTvShowId);
+  }
+
+  /******************** OTHER FUNCTIONS ********************/
   onActiveTabSelection(activeTab: string) : void {
     this.selectedMovieId = -1;
     this.activeTab = activeTab;
-    this.activeTab == 'movies' ? this.getUpcomingMovies(this.genreId) : this.getUpcomingTvShows(this.genreId);
+    this.activeTab == 'movies' ? this.getUpcomingMovies(this.genreId) : this.getUpcomingTVShows(this.genreId);
   }
 
   onGenreSelection(genreId: number): void {
     this.selectedMovieId = -1;
     this.genreId = genreId;
-    this.activeTab == 'movies' ? this.getUpcomingMovies(this.genreId) : this.getUpcomingTvShows(this.genreId);
+    this.activeTab == 'movies' ? this.getUpcomingMovies(this.genreId) : this.getUpcomingTVShows(this.genreId);
   }
 
   onSearchQuery(query: string): void {
     this.selectedMovieId = -1;
     this.searchQuery = query;
-    this.activeTab === 'movies' ? this.getUpcomingMovies(this.genreId) : this.getUpcomingTvShows(this.genreId);
+    this.activeTab === 'movies' ? this.getUpcomingMovies(this.genreId) : this.getUpcomingTVShows(this.genreId);
   }
 
-  selectMovie(id: number){
-    this.selectedMovieId = id;
-    this.getMovieDetails(this.selectedMovieId);
-  }
-
-  unselectMovie(){
+  back(){
     this.selectedMovieId = -1;
+    this.selectedTvShowId = -1;
   }
 }
--- src/app/services/themoviedb/themoviedb.service.ts
+++ src/app/services/themoviedb/themoviedb.service.ts
@@ -12,21 +12,28 @@ export class ThemoviedbService {
 
   constructor(private http: HttpClient) { }
 
-  getGenres(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}&language=en-US`);
-  }
-
+  /************** MOVIES APIs **************/
   getUpcomingMovies(genreId: number): Observable<any> {
     const currentDate = new Date().toISOString().split('T')[0];
     return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=${genreId}&sort_by=release_date.desc&primary_release_date.gte=${currentDate}`);
   }
 
+  getMovieDetails(movieId: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}&append_to_response=credits`);
+  }
+
+  /************** TV SHOWS APIs **************/
   getUpcomingTVShows(genreId: number): Observable<any> {
     const currentDate = new Date().toISOString().split('T')[0];
     return this.http.get(`${this.baseUrl}/discover/tv?api_key=${this.apiKey}&with_genres=${genreId}&sort_by=first_air_date.desc&first_air_date.gte=${currentDate}`);
   }  
 
-  getMovieDetails(movieId: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}&append_to_response=credits`);
+  getTVShowDetails(tvId: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/tv/${tvId}?api_key=${this.apiKey}&append_to_response=credits`);
   }
+
+  /************** OTHER APIs **************/
+  getGenres(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}&language=en-US`);
+  }  
 }
